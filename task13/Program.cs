cb1e687e20a30bc4f5279a5571b2ba824278c6c9
aleksander <petrov_records@mail.ru>
2022-10-21 12:56:40 +0400

    mod task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,7 +5,12 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-int index = 2;
-int array = number.ToString()[index] - '0';
-Console.WriteLine(array);
-
+if(number > 99)
+{
+    int index = 2;
+    int array = number.ToString()[index] - '0';
+    Console.WriteLine(array);
+}
+else{
+    Console.WriteLine("Третьей цифры нет");
+}
